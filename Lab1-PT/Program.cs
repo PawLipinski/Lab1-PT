49447ddbc23c185a3d46711bd7212b141e2c3ff1
Paweł Lipiński <p.lipinski20@gmail.com>
2018-03-22 12:48:15 +0100

    Some recursive method started

--- Lab1-PT/Program.cs
+++ Lab1-PT/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Lab1_PT
 {
@@ -10,6 +12,67 @@ namespace Lab1_PT
     {
         static void Main(string[] args)
         {
+            DirectoryInfo di = new DirectoryInfo("C:\\Users\\pawel.lipinski\\Downloads");
+            FileSystemInfo[] internals;
+
+            try
+            {
+                if (di.Exists)
+                {
+                    internals = di.GetFileSystemInfos();
+
+                    foreach (var item in internals)
+                    {
+                        if ((item.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            System.Console.WriteLine(item.Name);
+
+                            if (((DirectoryInfo)item).GetFileSystemInfos().Any())
+                            {
+                                Console.ResetColor();
+
+                                foreach (var element in ((DirectoryInfo)item).GetFileSystemInfos())
+                                {
+                                    System.Console.WriteLine("\t" + element.Name);
+                                    Console.ResetColor();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            System.Console.WriteLine(item.Name);
+                        }
+
+                        Console.ResetColor();
+
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("No such directory");
+            }
+        }
+
+        public bool RecursiveSubdirectoriesPrinter(FileSystemInfo analysed, int tabLength = 0)
+        {
+            if ((analysed.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                if (((DirectoryInfo)analysed).GetFileSystemInfos().Any())
+                {
+                    foreach (var item in ((DirectoryInfo)analysed).GetFileSystemInfos())
+                    {
+                        bool hasChildren = RecursiveSubdirectoriesPrinter(item);
+                    }
+                }
+            }
+
+            else
+            {
+                System.Console.WriteLine();
+                return false;
+            } 
         }
     }
 }
