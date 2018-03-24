e64d6639dc759b356d3b9757b79a956ff1764211
Paweł Lipiński <p.lipinski20@gmail.com>
2018-03-24 15:39:29 +0100

    Started assembly project

---
+++ DirectoryInfoExtender/DirectoryInfoExtender/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryInfoExtender
+{
+    public static class Class1
+    {
+        public static long FilesSize(this string path)
+        {
+            long size = 0;
+
+            DirectoryInfo di = new DirectoryInfo(@path);
+
+            foreach (var item in di.GetFiles())
+            {
+                size += (long)item.Length;
+            }
+
+            return size;
+        }
+    }
+}
